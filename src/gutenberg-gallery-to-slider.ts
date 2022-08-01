3d21a3196276f6af458f90ef7881c2bdb1cb676e
Crostiningaahhhh <info@ungaahhhh.xyz>
2022-08-01 14:32:49 +0900

    fix setIndex

--- src/gutenberg-gallery-to-slider.ts
+++ src/gutenberg-gallery-to-slider.ts
@@ -1,87 +1,121 @@
 export class WpGallery {
 	constructor(config) {
 		this.init(config);
-		if (this.storage.target) {
-			this.storage.target.outerHTML = `<div class="wp_gallery"><div class="wp_gallery_inner">${this.storage.target.outerHTML}</div></div>`;
-			this.init();
-			if (this.storage.scope) this.storage.gallery = this.getSelector(this.storage.scope, '.wp_gallery');
-			if (this.storage.gallery) this.storage.gallery.classList.add(this.config.effect);
-			if (this.storage.gallery) this.storage.wpGalleryInner = this.getSelector(this.storage.gallery, '.wp_gallery_inner');
-			this.storage.target.classList.add('wp_target');
-			this.storage.itemCount = this.storage.target.childElementCount;
-			if (this.config.thumb === 'dot') {
-				const wpGalleryThumbTemp = this.getTemp(this.storage.temp, 'wp_gallery_thumb_temp');
-				const wpGalleryThumb = this.getSelector(wpGalleryThumbTemp, '.wp_gallery_thumb');
-				if (this.config.thumb) wpGalleryThumb.classList.add(this.config.thumb);
-				for (let i = 0; i < this.storage.itemCount; i++) {
-					const wpGalleryThumbItemTemp = this.getTemp(this.storage.temp, 'wp_gallery_thumb_item_temp');
-					const wpGalleryThumbItem = this.getSelector(wpGalleryThumbItemTemp, '.wp_gallery_thumb_item');
-					wpGalleryThumb.appendChild(wpGalleryThumbItem);
-					wpGalleryThumbItem.addEventListener('click', (e) => {
-						this.setIndex(i, 'order');
-					});
-				}
-				if (this.storage.wpGalleryInner) this.storage.wpGalleryInner.appendChild(wpGalleryThumb);
-				this.storage.thumbItem = this.getSelectorAll(this.storage.gallery, '.wp_gallery_thumb_item');
-			} else {
-				const wpGalleryThumbTemp = this.getTemp(this.storage.temp, 'wp_gallery_thumb_temp');
-				const wpGalleryThumb = this.getSelector(wpGalleryThumbTemp, '.wp_gallery_thumb');
-				if (this.config.thumb) wpGalleryThumb.classList.add(this.config.thumb);
-				for (let i = 0; i < this.storage.itemCount; i++) {
-					const image = this.storage.target.children[i];
-					image.classList.add('wp_target_item');
-					const wpGalleryThumbItemTemp = this.getTemp(this.storage.temp, 'wp_gallery_thumb_item_temp');
-					const wpGalleryThumbItem = this.getSelector(wpGalleryThumbItemTemp, '.wp_gallery_thumb_item');
-					const wpGalleryThumbImgTemp = this.getTemp(this.storage.temp, 'wp_gallery_thumb_img_temp');
-					const img = this.getSelector(wpGalleryThumbImgTemp, 'img') as HTMLImageElement;
-					img.src = (this.getSelector(image, 'img') as HTMLImageElement).src;
-					wpGalleryThumbItem.appendChild(img);
-					wpGalleryThumb.appendChild(wpGalleryThumbItem);
-					wpGalleryThumbItem.addEventListener('click', (e) => {
-						this.setIndex(i, 'order');
-					});
-				}
-				const wpGalleryControlTemp = this.getTemp(this.storage.temp, 'wp_gallery_control_temp');
-				const prev = this.getSelector(wpGalleryControlTemp, '.prev');
-				const next = this.getSelector(wpGalleryControlTemp, '.next');
-				prev.addEventListener('click', (e) => {
-					this.setIndex(-1);
-				});
-				next.addEventListener('click', (e) => {
-					this.setIndex(1);
-				});
-				if (this.storage.gallery) this.storage.gallery.appendChild(wpGalleryThumb);
-				if (this.storage.wpGalleryInner) this.storage.wpGalleryInner.appendChild(wpGalleryControlTemp);
-				this.storage.thumbItem = this.getSelectorAll(this.storage.gallery, '.wp_gallery_thumb_item');
-			}
-			if (this.config.effect === 'fade') {
-				for (let i = 0; i < this.storage.itemCount; i++) {
-					const element = this.storage.target.children[i];
-					if (i === 0) {
-						element.classList.add('current');
-					}
-					element.addEventListener('animationend', (e) => {
-						const target = e.target as HTMLElement;
-						if (target.classList.contains('fadeOut')) target.classList.remove('fadeOut');
-						if (target.classList.contains('current')) target.classList.remove('current');
-						if (target.classList.contains('fadeIn')) {
-							target.classList.remove('fadeIn');
-							target.classList.add('current');
+		if (this.storage.target && this.storage.target.length > 0) {
+			for (let i = 0; i < this.storage.target.length; i++) {
+				let target = this.storage.target.item(i);
+				if (target) {
+					target.outerHTML = `<div class="wp_gallery"><div class="wp_gallery_inner">${target.outerHTML}</div></div>`;
+					this.init();
+					target = this.storage.target.item(i);
+					if (target) {
+						if (this.storage.scope)
+							this.storage.gallery = this.getSelector(this.storage.scope, '.wp_gallery');
+						if (this.storage.gallery) this.storage.gallery.classList.add(this.config.effect);
+						if (this.storage.gallery)
+							this.storage.wpGalleryInner = this.getSelector(this.storage.gallery, '.wp_gallery_inner');
+						target.classList.add('wp_target');
+						this.storage.itemCount = target.childElementCount;
+						if (this.config.thumb === 'dot') {
+							const wpGalleryThumbTemp = this.getTemp(this.storage.temp, 'wp_gallery_thumb_temp');
+							const wpGalleryThumb = this.getSelector(wpGalleryThumbTemp, '.wp_gallery_thumb');
+							if (this.config.thumb) wpGalleryThumb.classList.add(this.config.thumb);
+							for (let j = 0; j < this.storage.itemCount; j++) {
+								const wpGalleryThumbItemTemp = this.getTemp(
+									this.storage.temp,
+									'wp_gallery_thumb_item_temp'
+								);
+								const wpGalleryThumbItem = this.getSelector(
+									wpGalleryThumbItemTemp,
+									'.wp_gallery_thumb_item'
+								);
+								wpGalleryThumb.appendChild(wpGalleryThumbItem);
+								wpGalleryThumbItem.addEventListener('click', (e) => {
+									this.setIndex(i, j, 'order');
+								});
+							}
+							if (this.storage.wpGalleryInner) this.storage.wpGalleryInner.appendChild(wpGalleryThumb);
+							this.storage.thumbItem = this.getSelectorAll(
+								this.storage.gallery,
+								'.wp_gallery_thumb_item'
+							);
+						} else {
+							const wpGalleryThumbTemp = this.getTemp(this.storage.temp, 'wp_gallery_thumb_temp');
+							const wpGalleryThumb = this.getSelector(wpGalleryThumbTemp, '.wp_gallery_thumb');
+							if (this.config.thumb) wpGalleryThumb.classList.add(this.config.thumb);
+							for (let j = 0; j < this.storage.itemCount; j++) {
+								const image = target.children[j];
+								image.classList.add('wp_target_item');
+								const wpGalleryThumbItemTemp = this.getTemp(
+									this.storage.temp,
+									'wp_gallery_thumb_item_temp'
+								);
+								const wpGalleryThumbItem = this.getSelector(
+									wpGalleryThumbItemTemp,
+									'.wp_gallery_thumb_item'
+								);
+								const wpGalleryThumbImgTemp = this.getTemp(
+									this.storage.temp,
+									'wp_gallery_thumb_img_temp'
+								);
+								const img = this.getSelector(wpGalleryThumbImgTemp, 'img') as HTMLImageElement;
+								img.src = (this.getSelector(image, 'img') as HTMLImageElement).src;
+								wpGalleryThumbItem.appendChild(img);
+								wpGalleryThumb.appendChild(wpGalleryThumbItem);
+								wpGalleryThumbItem.addEventListener('click', (e) => {
+									this.setIndex(i, j, 'order');
+								});
+							}
+							const wpGalleryControlTemp = this.getTemp(this.storage.temp, 'wp_gallery_control_temp');
+							const prev = this.getSelector(wpGalleryControlTemp, '.prev');
+							const next = this.getSelector(wpGalleryControlTemp, '.next');
+							prev.dataset.targetIndex = String(i);
+							next.dataset.targetIndex = String(i);
+							prev.addEventListener('click', (e) => {
+								this.setIndex(i, -1);
+							});
+							next.addEventListener('click', (e) => {
+								this.setIndex(i, 1);
+							});
+							if (this.storage.gallery) this.storage.gallery.appendChild(wpGalleryThumb);
+							if (this.storage.wpGalleryInner)
+								this.storage.wpGalleryInner.appendChild(wpGalleryControlTemp);
+							this.storage.thumbItem = this.getSelectorAll(
+								this.storage.gallery,
+								'.wp_gallery_thumb_item'
+							);
 						}
-					});
+						if (this.config.effect === 'fade') {
+							for (let j = 0; j < this.storage.itemCount; j++) {
+								const element = target.children[j];
+								if (j === 0) {
+									element.classList.add('current');
+								}
+								element.addEventListener('animationend', (e) => {
+									const target = e.target as HTMLElement;
+									if (target.classList.contains('fadeOut')) target.classList.remove('fadeOut');
+									if (target.classList.contains('current')) target.classList.remove('current');
+									if (target.classList.contains('fadeIn')) {
+										target.classList.remove('fadeIn');
+										target.classList.add('current');
+									}
+								});
+							}
+						}
+						this.setIndex(i, 0, 'order');
+						if (this.config.interval) {
+							this.setInterval();
+							if (this.storage.gallery)
+								this.storage.gallery.addEventListener('mouseenter', () => {
+									this.abortInterval();
+								});
+							if (this.storage.gallery)
+								this.storage.gallery.addEventListener('mouseleave', () => {
+									this.setInterval();
+								});
+						}
+					}
 				}
-			}
-			this.setIndex(0, 'order');
-			if (this.config.interval) {
-				this.setInterval();
-				if (this.storage.gallery)
-					this.storage.gallery.addEventListener('mouseenter', () => {
-						this.abortInterval();
-					});
-				if (this.storage.gallery)
-					this.storage.gallery.addEventListener('mouseleave', () => {
-						this.setInterval();
-					});
 			}
 		}
 	}
@@ -114,7 +148,7 @@ export class WpGallery {
 		}
 		this.storage.temp = this.getTemp(document, 'wp_gallery_temp');
 		this.storage.scope = this.getSelector(document, this.config.scope);
-		this.storage.target = this.getSelector(this.storage.scope, this.config.target);
+		this.storage.target = this.getSelectorAll(this.storage.scope, this.config.target);
 	};
 	getTemp = (parent: any, id: string): HTMLElement => {
 		return parent.getElementById(id).content.cloneNode(true) as HTMLElement;
@@ -125,7 +159,7 @@ export class WpGallery {
 	getSelectorAll = (parent: any, query: string): HTMLCollection => {
 		return parent.querySelectorAll(query);
 	};
-	setIndex = (num, mode?) => {
+	setIndex = (targetIndex: number, num: number, mode?: 'reverse' | 'order') => {
 		const last = this.storage.current;
 		if (mode) {
 			switch (mode) {
@@ -151,16 +185,18 @@ export class WpGallery {
 			case 'fade':
 				if (!this.storage.isEffectFirst) {
 					if (this.storage.target) {
-						this.storage.target.children[last].classList.remove('fadeIn');
-						this.storage.target.children[last].classList.add('fadeOut');
-						this.storage.target.children[this.storage.current].classList.remove('fadeOut');
-						this.storage.target.children[this.storage.current].classList.add('fadeIn');
+						this.storage.target[targetIndex].children[last].classList.remove('fadeIn');
+						this.storage.target[targetIndex].children[last].classList.add('fadeOut');
+						this.storage.target[targetIndex].children[this.storage.current].classList.remove('fadeOut');
+						this.storage.target[targetIndex].children[this.storage.current].classList.add('fadeIn');
 					}
 				}
 				this.storage.isEffectFirst = false;
 				break;
 			default:
-				if (this.storage.target) this.storage.target.scrollLeft = this.storage.target.offsetWidth * this.storage.current;
+				if (this.storage.target)
+					this.storage.target[targetIndex].scrollLeft =
+						this.storage.target[targetIndex].offsetWidth * this.storage.current;
 				break;
 		}
 		if (this.config.thumb) {
@@ -176,7 +212,11 @@ export class WpGallery {
 	};
 	setInterval = () => {
 		this.storage.interval = setInterval(() => {
-			this.setIndex(1);
+			if (this.storage.target && this.storage.target.length > 0) {
+				for (let i = 0; i < this.storage.target.length; i++) {
+					this.setIndex(i, 1);
+				}
+			}
 		}, this.config.interval);
 	};
 	abortInterval = () => {
@@ -199,7 +239,7 @@ type storage = {
 	isEffectFirst: boolean;
 	itemCount: number;
 	scope: HTMLElement | null;
-	target: HTMLElement | null;
+	target: HTMLCollection | null;
 	temp: HTMLElement | null;
 	thumbItem: HTMLCollection | null;
 	wpGalleryInner: HTMLElement | null;
--- src/sample.ts
+++ src/sample.ts
@@ -1,6 +1,8 @@
 import { WpGallery } from './gutenberg-gallery-to-slider';
 window.addEventListener('DOMContentLoaded', async () => {
-	const response = await fetch('https://pixabay.com/api/?key=28916998-477d57555c1b9cf691a3084d9&category=animals&per_page=8');
+	const response = await fetch(
+		'https://pixabay.com/api/?key=28916998-477d57555c1b9cf691a3084d9&category=animals&per_page=8'
+	);
 	const json = await response.json();
 	const sample = document.querySelectorAll<HTMLUListElement>('.sample');
 	for (let i = 0; i < sample.length; i++) {
@@ -13,6 +15,7 @@ window.addEventListener('DOMContentLoaded', async () => {
 		target.innerHTML = HTML;
 	}
 	const wpGalleryConfig = {
+		interval: 4000,
 		scope: '#scroll',
 		target: 'ul',
 	};
