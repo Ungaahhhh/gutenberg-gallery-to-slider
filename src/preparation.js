837fab5dc9ed4d23c894dc1ccc8a850714008278
Crostiningaahhhh <info@ungaahhhh.xyz>
2022-08-08 18:29:37 +0900

    try preparation.js

---
+++ src/preparation.js
@@ -0,0 +1,8 @@
+const response = await fetch(
+	'https://pixabay.com/api/?key=28916998-477d57555c1b9cf691a3084d9&category=animals&per_page=8'
+);
+const json = await response.json();
+for (let j = 0; j < json.hits.length; j++) {
+	const item = json.hits[j];
+	console.log(item.largeImageURL);
+}
